13e0ffb5de13da4b32f0890b5582a37562a091e4
ParthKunjadiya <parthkunjadiya3@gmail.com>
2024-05-17 11:40:50 +0530

    add city and state in address

--- repository/address.js
+++ repository/address.js
@@ -1,17 +1,17 @@
 const db = require('../util/db');
 
-const insertAddress = async ({ user_id, name, mobileNumber, email, address, pinCode }) => {
+const insertAddress = async ({ user_id, name, mobileNumber, email, address, pinCode, city, state }) => {
     let sql = `INSERT INTO address SET ?`
 
-    let params = { user_id, name, mobile_number: mobileNumber, email, address, pin_code: pinCode }
+    let params = { user_id, name, mobile_number: mobileNumber, email, address, pin_code: pinCode, city, state }
     return await db.query(sql, params)
 }
 
 const insertOrderAddress = async (addressDetail) => {
-    const { name, mobile_number, email, address, pin_code } = addressDetail
+    const { name, mobile_number, email, address, pin_code, city, state } = addressDetail
     let sql = `INSERT INTO orderAddress SET ?`
 
-    let params = { name, mobile_number, email, address, pin_code }
+    let params = { name, mobile_number, email, address, pin_code, city, state }
     return await db.query(sql, params)
 }
 
@@ -19,7 +19,7 @@ const getAddress = async (key) => {
     const keys = Object.keys(key);
     const values = Object.values(key);
 
-    let sql = `SELECT id, name, mobile_number, email, address, pin_code FROM address WHERE `;
+    let sql = `SELECT id, name, mobile_number, email, address, pin_code, city, state FROM address WHERE `;
     sql += keys.map(key => `${key} = ?`).join(' AND ');
 
     return await db.query(sql, values);
