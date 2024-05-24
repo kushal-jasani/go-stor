3043e5a5d82c7c5ced910a914822bd0035dc8c2e
ParthKunjadiya <parthkunjadiya3@gmail.com>
2024-05-24 14:11:46 +0530

    file structure changed

--- src/repository/user.js
+++ src/repository/user.js
@@ -1,8 +1,8 @@
-const db=require('../util/db');
-const findUserDetails=async (id)=>{
+const db = require('../util/db');
+const findUserDetails = async (id) => {
     return await db.query("select name,email,phoneno from users where id = ?", [id]);
 }
 
-module.exports={
+module.exports = {
     findUserDetails
 }
---
+++ src/routes/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const authRoutes = require('./auth');
+const homeRoutes = require('./home');
+const userRoutes = require('./user');
+const productRoutes = require('./products');
+const addressRoutes = require('./address');
+const orderRoutes = require('./order');
+const couponRoutes = require('./coupons');
+const storeRoutes = require('./store');
+
+const router = express.Router();
+
+router.use('/home', homeRoutes);
+router.use('/auth', authRoutes);
+router.use('/user', userRoutes);
+router.use('/shop', productRoutes);
+router.use('/orders', addressRoutes);
+router.use('/orders', orderRoutes);
+router.use('/coupons', couponRoutes);
+router.use('/store', storeRoutes);
+
+module.exports = router;
